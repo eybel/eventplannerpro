e46bbf31b18051c35be10ba852a08cee3bb3f6b2
Abel Murua <abelmurua87@gmail.com>
2023-09-09 00:54:16 -0300

    updated menu

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -3,23 +3,23 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const navItems = ["Home", "About", "Contact", "Blog", "Careers"];
+const navItems = ["Home", "EventPlanner", "Pricing", "Contact Us"];
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div className="fixed z-50 top-0 w-full bg-white">
-        <nav className="flex justify-between items-center z-20 px-20 py-4">
+      <div className="fixed top-0 z-50 w-full bg-white">
+        <nav className="z-20 flex items-center justify-between px-20 py-4">
           <div>
             <Image src="/images/logo.png" width="100" height="100" alt="logo" />
           </div>
 
-          <div className="hidden lg:block text-sm text-neutral-grayish-blue">
+          <div className="hidden text-sm lg:block text-neutral-grayish-blue">
             {navItems.map((navItem, index) => (
               <a
                 key={index}
-                className="mx-3 py-5 hover:gradient-border-bottom"
+                className="py-5 mx-3 hover:gradient-border-bottom"
                 href="#"
               >
                 {navItem}
@@ -27,7 +27,7 @@ export default function Navbar() {
             ))}
           </div>
 
-          <button className="hidden lg:block bg-primary-lime-green px-7 py-3 rounded-full text-neutral-white text-xs bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan hover:button-brightness focus:outline-none focus:ring ring-green-400">
+          <button className="hidden py-3 text-xs rounded-full lg:block bg-primary-lime-green px-7 text-neutral-white bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan hover:button-brightness focus:outline-none focus:ring ring-green-400">
             Book Now!
           </button>
 
@@ -58,7 +58,7 @@ export default function Navbar() {
         className={`fixed inset-0 z-30 bg-gray-800
       bg-opacity-50 ${isOpen ? "block" : "hidden"}`}
       >
-        <div className="bg-white text-primary-dark-blue flex flex-col text-center mx-5 my-20 py-4 rounded">
+        <div className="flex flex-col py-4 mx-5 my-20 text-center bg-white rounded text-primary-dark-blue">
           {navItems.map((navItem) => (
             <a key={navItem} className="py-2" href="#">
               {navItem}
