8786635492fb79e679b6a91f8f2335d4663a4be3
Abel Murua <abelmurua87@gmail.com>
2023-09-08 21:06:56 -0300

    decided to also impement a popup

--- components/popup.js
+++ components/popup.js
@@ -2,11 +2,11 @@ const Popup = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-30">
       <div className="fixed top-0 left-0 w-full h-full bg-black-50"></div>
-      <div className="relative p-4 bg-white rounded-lg shadow-lg">
+      <div className="relative bg-white rounded-lg shadow-lg p-10">
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
           onClick={onClose}
         >
           Close
@@ -14,7 +14,10 @@ const Popup = ({ isOpen, onClose }) => {
         {/* Popup content goes here */}
         <div className="p-4">
           <h2 className="text-2xl font-semibold mb-4">Popup Title</h2>
-          <p>Popup content</p>
+          <p>
+            Popup content goes here. We can show important information about
+            this EventPlanner Pro
+          </p>
         </div>
       </div>
     </div>
